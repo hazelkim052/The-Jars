080832968d23c469ba5e1621e0ffa8d5700f8e12
haleysong576 <haleysong576@gmail.com>
2021-09-18 16:06:17 -0400

    fix

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { HappinessComponent } from './component/happiness/happiness.component';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'happiness', component: HappinessComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HappinessComponent } from './happiness/happiness.component';
-
-import { RouterModule } from '@angular/router';
-
-const routes = [
-  { path: '', component: HomeComponent },
-  { path: '/happiness', component: HappinessComponent },
-];
+import { HomeComponent } from './component/home/home.component';
+import { HappinessComponent } from './component/happiness/happiness.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HappinessComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
