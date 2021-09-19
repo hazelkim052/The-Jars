e9a8bd2b2e923491d02206307a335dd795ff99e9
hshj7900 <hshj7900@gmail.com>
2021-09-18 20:28:05 -0400

    happiness page

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HappinessComponent],
@@ -18,6 +22,11 @@ import { MatInputModule } from '@angular/material/input';
     BrowserAnimationsModule,
     MatCardModule,
     MatInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Client/src/app/component/happiness/happiness.component.ts
+++ Client/src/app/component/happiness/happiness.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./happiness.component.css'],
 })
 export class HappinessComponent implements OnInit {
+  picker: any;
   constructor() {}
 
   ngOnInit(): void {}
