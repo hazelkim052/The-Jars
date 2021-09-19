2db2c63c18d3503e5210f5dc64db2befb66e05b5
hshj7900 <hshj7900@gmail.com>
2021-09-19 02:09:42 -0400

    dayjs update

--- Client/src/app/component/motivate/motivate.component.ts
+++ Client/src/app/component/motivate/motivate.component.ts
@@ -1,28 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { HappyService } from '../../service/happy.service';
 import { Router } from '@angular/router';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-motivate',
   templateUrl: './motivate.component.html',
-  styleUrls: ['./motivate.component.css']
+  styleUrls: ['./motivate.component.css'],
 })
 export class MotivateComponent implements OnInit {
-
   date: any;
   story: any;
   constructor(public happyService: HappyService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
-  try () {
-    this.router.navigateByUrl('/')
+  ngOnInit(): void {}
+  try() {
+    this.router.navigateByUrl('/');
   }
   post(story: any, date: any) {
-    console.log('post test');
-    console.log('story', story);
-    console.log('date', date);
+    date = dayjs(date).format('MMMM DD, YYYY');
     this.happyService.postMotivation(story, date);
   }
-
 }
--- Client/src/app/component/sad/sad.component.ts
+++ Client/src/app/component/sad/sad.component.ts
@@ -1,28 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { HappyService } from '../../service/happy.service';
 import { Router } from '@angular/router';
+import * as dayjs from 'dayjs';
 
 @Component({
   selector: 'app-sad',
   templateUrl: './sad.component.html',
-  styleUrls: ['./sad.component.css']
+  styleUrls: ['./sad.component.css'],
 })
 export class SadComponent implements OnInit {
-
   date: any;
   story: any;
   constructor(public happyService: HappyService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
-  try () {
-    this.router.navigateByUrl('/')
+  ngOnInit(): void {}
+  try() {
+    this.router.navigateByUrl('/');
   }
   post(story: any, date: any) {
-    console.log('post test');
-    console.log('story', story);
-    console.log('date', date);
+    date = dayjs(date).format('MMMM DD, YYYY');
     this.happyService.postSadness(story, date);
   }
-
 }
